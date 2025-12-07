a3f35d8f590f8d24a3b30f91a523ce8acdda0ee7
Jose Rodrigues <ing.joserodrigues@gmail.com>
2025-12-07 18:27:20 -0300

    Swap merge sorted arrays solution and stub files
    Move solution to _solution.zig and stub to main file

--- problems/two-pointers/easy/04_merge_sorted_array/04_merge_sorted_array_solution.zig
+++ problems/two-pointers/easy/04_merge_sorted_array/04_merge_sorted_array_solution.zig
@@ -1,8 +1,17 @@
 const std = @import("std");
 
 pub fn mergeSortedArrays(nums1: []i32, m: usize, nums2: []const i32, n: usize) void {
-    _ = nums1;
-    _ = m;
-    _ = nums2;
-    _ = n;
+    var p1: usize = m;
+    var p2: usize = n;
+    var p: usize = m + n;
+    while (p2 > 0) {
+        p -= 1;
+        if (p1 > 0 and nums1[p1 - 1] > nums2[p2 - 1]) {
+            p1 -= 1;
+            nums1[p] = nums1[p1];
+        } else {
+            p2 -= 1;
+            nums1[p] = nums2[p2];
+        }
+    }
 }
